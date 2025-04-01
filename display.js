0bc04cec722c05a24635ccd4e22d60b6d5ba8e2f
Vitinho <victorhugopereiralemos6@gmail.com>
2025-04-01 11:21:39 -0300

    ikipjiok
    fdsjgafdjgshadfj

--- display.js
+++ display.js
@@ -1,14 +1,1 @@
-// Pegando o elemento de áudio e o botão
-var audioElement = document.getElementById("meuAudio");
-var playButton = document.getElementById("Ia");  // O ID agora é "botao", como no HTML
 
-// Adicionando evento de clique ao botão
-botao.addEventListener("click", function() {
-    if (audio.paused) {
-        audio.play();
-        botao.textContent = "Pausar";
-    } else {
-        audio.pause();
-        botao.textContent = "Tocar";
-    }
-});
